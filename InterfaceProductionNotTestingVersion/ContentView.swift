ea05e55a7e6794bb0d559562d99a39e9aeceae31
Tom <twu@lcs.on.ca>
2022-11-17 09:49:29 -0500

    basic structure

--- InterfaceProductionNotTestingVersion/ContentView.swift
+++ InterfaceProductionNotTestingVersion/ContentView.swift
@@ -10,10 +10,53 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            NavigationView{
+                List{
+                    HStack{
+                        
+                        VStack{
+                            Text("need add")
+                            Text("Vancouver")
+                        }
+                        Spacer()
+                        Text("0:00")
+                            .frame(height: 100)
+                    }
+                    
+                    HStack{
+                        VStack{
+                            Text("need add")
+                            Text("Toronto")
+                        }
+                        Spacer()
+                        Text("0:01")
+                            .frame(height: 100)
+                    }
+                    
+                    HStack{
+                        VStack{
+                            Text("need add")
+                            Text("Beijing")
+                        }
+                        Spacer()
+                        Text("0:02")
+                            .frame(height: 100)
+                    }
+                    
+                    HStack{
+                        VStack{
+                            Text("need add")
+                            Text("Beijing")
+                        }
+                        Spacer()
+                        Text("0:02")
+                            .frame(height: 100)
+                    }
+                }
+                .navigationTitle("WorldClock")
+                .toolbar{EditButton()}
+                .toolbar{UIButton(.plus)}
+            }
         }
         .padding()
     }
@@ -21,6 +64,36 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        TabView(selection: Binding.constant(3)){
+            Text("World Clock")
+                .tabItem{
+                    Image(systemName: "globe")
+                    Text("World Clock")
+                }
+                .tag(1)
+            
+            Text("Alarm")
+                .tabItem{
+                    Image(systemName: "alarm.fill")
+                    Text("Alarm Clock")
+                }
+                .tag(2)
+            
+            ContentView()
+                .tabItem{
+                    Image(systemName: "stopwatch.fill")
+                    Text("Stop Watch")
+                }
+                .tag(3)
+            
+            Text("Timer")
+                .tabItem{
+                    Image(systemName: "timer")
+                    Text("Timer")
+                }
+                .tag(4)
+        }
+        .accentColor(.orange)
+        .preferredColorScheme(.dark)
     }
 }
