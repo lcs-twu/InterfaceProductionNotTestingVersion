afadf933eb72945b77abeaf830d34005adfe85fa
Tom <twu@lcs.on.ca>
2022-11-17 09:32:37 -0500

    Initial Commit

---
+++ InterfaceProductionNotTestingVersion/InterfaceProductionNotTestingVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceProductionNotTestingVersionApp.swift
+//  InterfaceProductionNotTestingVersion
+//
+//  Created by Tom Wu on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceProductionNotTestingVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
